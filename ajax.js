df904676b54160d87f1dc1ea74fd55838517cc0d
xcatx666 <116883975+xcatx666@users.noreply.github.com>
2022-10-28 07:16:12 -0400

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+var peticion = false;
+var  testPasado = false;
+try {
+  peticion = new XMLHttpRequest();
+  } catch (trymicrosoft) {
+  try {
+  peticion = new ActiveXObject("Msxml2.XMLHTTP");
+  } catch (othermicrosoft) {
+  try {
+  peticion = new ActiveXObject("Microsoft.XMLHTTP");
+  } catch (failed) {
+  peticion = false;
+  }
+  }
+}
+if (!peticion)
+alert("ERROR AL INICIALIZAR!");
+
+function ajaxevento (url, element_id) {
+    //Obtenemos el contenido del div donde se cargaran los resultados
+    var element =  document.getElementById(element_id);
+    //construimos la url definitiva
+    var fragment_url = url;
+    //abrimos la url
+    peticion.open("GET", fragment_url);
+    peticion.onreadystatechange = function() {
+        if (peticion.readyState == 4) {
+	//escribimos la respuesta
+	element.innerHTML = peticion.responseText;
+        }
+    }
+   peticion.send(null);
+}
